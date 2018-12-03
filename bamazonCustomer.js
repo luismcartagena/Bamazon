cc55d26ab4b2ba2c39d7cf24c5943d77eca16d32
Luis Cartagena <luiscartagena2017@u.northwestern.edu>
2018-12-02 21:13:41 -0600

    updated table/products

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,59 +3,45 @@ const mysql = require("mysql");
 const table = require("table").table;
 
 const connection = mysql.createConnection({
-    password: "",
-    user: "root",
     host: "localhost",
     port: 3306,
+    user: "guest",
+    password: "Sunday2018!",
     database: "bamazon_db"
 });
 
-connection.connect(err => {
+// Load Bamazon
+const loadApp = () => {
+    connection.connect(err => {
     if (err) {
         throw err;
     }
-
-    start();
-})
-
-
-const products = [
-    {
-        id: 1,
-        department_name: "Shoes",
-        name: "Jordan 13 Retro",
-        price: 220.00,
-        stock_quantity: 100
-    },
-    {
-        id: 2,
-        department_name: "Shoes",
-        name: "Jordan 11",
-        price: 230.00,
-        stock_quantity: 100
-
-    },
-    {
-        id: 3,
-        department_name: "Shoes",
-        name: "Jordan 7 retro",
-        price: 220.00,
-        stock_quantity: 100
-
-    }
-]
-
-
-const start = () => {
-    // running this application will first diaplay all of the items available for sale
-    // include the ids, names, and prices of products for sale.
+    console.log("connected as id " + connection.threadId + "\n");
     displayProducts();
-    // app prompt user
-    getUserAction();
+});
 }
 
+
+// Display the main table of products
+const displayProducts = () => {
+    // connect to the database
+    connection.query("SELECT  FROM products", (err, result) => {
+        if (err) {
+            throw err;
+        }
+    // })
+
+    // display each product (can use a package for nice display)
+    const data = products.map(product => [product.id, product.department_name, product.name, product.price, product.stock_quantity]);
+    console.log(table(data));
+    getUserAction();
+    });
+}
+
+
+
+// Input and data validation 
 const getUserAction = () => {
-    // the app should then 
     inquirer.prompt([
         {
             name: "choice",
@@ -65,7 +51,7 @@ const getUserAction = () => {
         {
             name: "quantity",
             type: "input",
-            message: "How many:"
+            message: "How many would you like:"
         }
     ]).then(answers => {
         // safe guard that they enter an existing ID
@@ -81,18 +67,10 @@ const getUserAction = () => {
         // switch(answers.choice) {
         //     // case statements
         // }
-
+        getUserAction();
         console.log(answers + "\n\n\n\n\n");
         // connect to the database
-        start();
     })
 }
-const displayProducts = () => {
-    // connect to the database
-    // select * from products;
-    // display each product (can use a package for nice display)
-    const data = products.map(product => [product.id, product.department_name, product.name, product.price, product.stock_quantity]);
-    console.log(table(data));
-}
 
-start();
+loadApp();
--- bamazon_db.sql
+++ bamazon_db.sql
@@ -6,19 +6,24 @@ USE bamazon_db;
 
 CREATE TABLE products (
 	id INT NOT NULL AUTO_INCREMENT,
-	department_name VARCHAR(255) NULL,
-    name VARCHAR(255) NULL,
-	price INT NULL,
-	stock_quantity INT NULL,
+	department_name VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+	price DECIMAL(10, 2) NOT NULL,
+	stock_quantity INT(10) NOT NULL,
 	PRIMARY KEY (id)
 );
 
 
-INSERT INTO products (department_name, name, price, stock_quantity)
-VALUES ("Shoes", "Jordan 13 Retro", 220, 100);
+INSERT INTO products (department_name, name, price, stock_quantity) VALUES 
+("Groceries", "Avocado", 1.50, 100),
+("Groceries", "Tortillas (12)", 3.00, 60),
+("Shoes", "KEEN Hiking Boots", 150.00, 100),
+("Camping", "Kelty Monarch Tent", 169.99, 100),
+("Outdoors", "OUTAD Traction Cleats", 62.99, 15),
+("Socks", "Darn Tough Socks", 24.99, 50),
+("Books", "Borderland: The New Mestiza", 18.77, 999),
+("Books", "The Elegant Universe", 21.20, 120),
+("Electronics", "Nintendo Switch", 299.90, 5),
+("Computers", "Macbook Pro 2018", 9999, 1);
 
-INSERT INTO products (department_name, name, price, stock_quantity)
-VALUES ("Shoes", "Jordan 11", 230, 100);
-
-INSERT INTO products (department_name, name, price, stock_quantity)
-VALUES ("Shoes", "Jordan 7 Retro", 220, 100);
+SELECT * FROM products;
