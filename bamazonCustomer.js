913d932807a8abd8a280345cf98bf806a9473e4f
Luis Cartagena <luiscartagena2017@u.northwestern.edu>
2018-12-10 00:18:17 -0600

    updated input validation

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -41,78 +41,96 @@ const displayProducts = () => {
       ` id    Department    Product Name                 Cost($)  Stock`
     );
     console.log(table(data));
-    getUserAction();
+    // getUserAction();
+    //     getUserAction();
+    //   });
+    // };
+
+    // const getUserAction = (array) => {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "choice",
+          message: "Please enter the id of the product you wish to purchase:",
+          validate: function(value) {
+            var valid = !isNaN(parseFloat(value));
+            return valid || "Please enter a number";
+          },
+          filter: Number
+        },
+        {
+          type: "input",
+          name: "quantity",
+          message: "How many would you like:",
+          validate: function(value) {
+            var valid = !isNaN(parseFloat(value));
+            return valid || "Please enter a number";
+          },
+          filter: Number
+        }
+      ])
+      .then(answers => {
+        // safe guard that they enter an existing ID
+        connection.query(
+          "SELECT id, stock_quantity FROM products WHERE ?",
+          [{name: answers.choice}],
+          (err, result) => {
+            if (err) {
+              throw err;
+            }
+            if (answers.quantity > result[0].stock_quantity) {
+              console.log(
+                `There is only ${
+                  result[0].stock_quantity
+                } units of that product currently in stock. Please enter a different amount to buy.`
+              );
+              displayProducts();
+            } else {
+              let newQuantity = result[0].stock_quantity - answers.quantity;
+              updateStock(answers.quantity, newQuantity, result[0].id);
+            }
+          }
+        );
+      });
   });
 };
 
-// Input and data validation
-const getUserAction = () => {
-  inquirer
-    .prompt([
-      {
-        name: "choice",
-        type: "input",
-        message: "Please enter the id of the product you wish to purchase:"
-      },
-      {
-        name: "quantity",
-        type: "input",
-        message: "How many would you like:"
-      }
-    ])
-    .then(answers => {
-      // safe guard that they enter an existing ID
-      // for loop, do a filter
-      const isValid = data.filter(data => data.id === answers.choice).length > 0;
-        for (isValid)
-      // // IF IT IS A CORRECT ANSWER PROMPT THEM FOR THE QUANTITY
-      // inquirer.prompt([
+// Update stock_quantity
 
-      // ]).then(quantity => {
-      //     // don't name inquirere prompt the same name (eg. answers)
-      // })
-      // switch(answers.choice) {
-      //     // case statements
-      // }
-      getUserAction();
-      console.log(answers + "\n\n\n\n\n");
-      // connect to the database
-    });
-};
+// const updateStock = () => {
+//     // connect to the database
+//     connection.query(
+//       "UPDATE products SET stock_quantity = ? WHERE id = ?",
+//       [req.body.stock_quantity, req.params.id],
+//       function(err, result) {
+//         if (err) {
+//           throw err;
+//         } else if (result.changedRows === 0) {
+//           // If no rows were changed, then the ID must not exist, so 404
+//           console.log("id does not exist");
+//           displayProducts();
+//         }
+//         result.status(200).end();
+//       }
+//     );
+//   };
 
 // Create a new movie
-app.post("/movies", function(req, res) {
-  connection.query(
-    "INSERT INTO movies (movie) VALUES (?)",
-    [req.body.movie],
-    function(err, result) {
-      if (err) {
-        return res.status(500).end();
-      }
+// app.post("/movies", function(req, res) {
+//   connection.query(
+//     "INSERT INTO movies (movie) VALUES (?)",
+//     [req.body.movie],
+//     function(err, result) {
+//       if (err) {
+//         return res.status(500).end();
+//       }
 
-      // Send back the ID of the new movie
-      res.json({ id: result.insertId });
-      console.log({ id: result.insertId });
-    }
-  );
-});
-
-// Update a movie
-app.put("/movies/:id", function(req, res) {
-  connection.query(
-    "UPDATE movies SET movie = ? WHERE id = ?",
-    [req.body.movie, req.params.id],
-    function(err, result) {
-      if (err) {
-        // If an error occurred, send a generic server failure
-        return res.status(500).end();
-      } else if (result.changedRows === 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      }
-      res.status(200).end();
-    }
-  );
-});
+//       // Send back the ID of the new movie
+//       res.json({ id: result.insertId });
+//       console.log({ id: result.insertId });
+//     }
+//   );
+// });
 
 loadApp();
